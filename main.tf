c6320293042fc857346cef0086a3bc20057ee57c
Ajay Thomas <ajgcplearning@gmail.com>
2024-09-24 16:07:45 +0100

    Updated Storage Values

--- main.tf
+++ main.tf
@@ -1,21 +1,37 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  address_space       = [var.vnet_address_space]
-  location            = var.location
+module "network" {
+  source              = "./modules/network"
+  vnet_name           = var.vnet_name
+  vnet_address_space  = var.vnet_address_space
   resource_group_name = var.resource_group_name
+  location            = var.location
 }
 
-resource "azurerm_subnet" "gateway_subnet" {
-  name                 = "Gateway-Subnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.10.0.0/29"]
+module "vm_web" {
+  source              = "./modules/vm"
+  vm_name             = "Web-VM1"
+  vm_size             = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  subnet_id           = module.network.web_subnet_id  # Pass Web Subnet ID
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
 
-resource "azurerm_subnet" "ad_subnet" {
-  name                 = "AD-Subnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.10.0.8/27"]
+module "vm_app" {
+  source              = "./modules/vm"
+  vm_name             = "App-VM1"
+  vm_size             = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  subnet_id           = module.network.app_subnet_id  # Pass App Subnet ID
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
-# Other subnets as required...
+
+module "storage" {
+  source              = "./modules/storage"
+  storage_account_name = var.storage_account_name
+  resource_group_name  = var.resource_group_name
+  location             = var.location
+  app_subnet_id        = module.network.app_subnet_id  # Pass the App Subnet ID
+}
--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -9,3 +9,12 @@ output "gateway_subnet_id" {
   description = "The ID of the gateway subnet."
   value       = azurerm_subnet.gateway_subnet.id
 }
+output "web_subnet_id" {
+  description = "The ID of the Web Subnet"
+  value       = azurerm_subnet.web_subnet.id
+}
+
+output "app_subnet_id" {
+  description = "The ID of the App Subnet"
+  value       = azurerm_subnet.app_subnet.id
+}
